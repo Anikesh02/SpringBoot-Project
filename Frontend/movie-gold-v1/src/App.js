a8779bcb8dd6d7c855c4640a490ec4e8f3ca1273
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-05-27 22:40:03 +0530

    Add and Get Reviews with HTTP Requests

--- Frontend/movie-gold-v1/src/App.js
+++ Frontend/movie-gold-v1/src/App.js
@@ -5,40 +5,68 @@ import api from './api/axiosConfig';
 import Layout from './components/Layout';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import NotFound from './components/notFound/NotFound';
+import Reviews from './components/reviews/Reviews';
 import Trailer from './components/trailer/Trailer';
-
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
-  const getMovies = async () => {
+  const getMovies = async () =>{
+    
+    try
+    {
 
-    try {
-      const response = await api.get('/api/v1/movies');
+      const response = await api.get("/api/v1/movies");
+
       setMovies(response.data);
 
-    } catch (err) {
+    } 
+    catch(err)
+    {
       console.log(err);
     }
-    
+  }
+
+  const getMovieData = async (movieId) => {
+     
+    try 
+    {
+        const response = await api.get(`/api/v1/movies/${movieId}`);
+
+        const singleMovie = response.data;
+
+        setMovie(singleMovie);
+
+        setReviews(singleMovie.reviews);
+        
+    } 
+    catch (error) 
+    {
+      console.error(error);
+    }
+
   }
 
   useEffect(() => {
     getMovies();
-  }, []);
-
-
+  },[])
 
   return (
     <div className="App">
-      <Header />
+      <Header/>
       <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route path='/' element={<Home movies={movies}/>}></Route>
-          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
-        </Route>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            <Route path="*" element = {<NotFound/>}></Route>
+          </Route>
       </Routes>
+
     </div>
   );
 }
