408689c9fdcfa66289df4e4500256adf06d47fe6
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-05-27 22:09:09 +0530

    Created Navbar and Trailer Components

--- Frontend/movie-gold-v1/src/App.js
+++ Frontend/movie-gold-v1/src/App.js
@@ -3,7 +3,9 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import api from './api/axiosConfig';
 import Layout from './components/Layout';
+import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Trailer from './components/trailer/Trailer';
 
 
 function App() {
@@ -30,10 +32,11 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path='/' element={<Layout/>}>
           <Route path='/' element={<Home movies={movies}/>}></Route>
-
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
         </Route>
       </Routes>
     </div>
